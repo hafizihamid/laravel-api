8ac845bc1425d2b2c1f1339fccfedf7a0b30f67b
hafizi hamid <mohamadhafizihamid@gmail.com>
2022-03-28 23:28:38 +0800

    Add config file

---
+++ config/staticdata.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'country_code' => '63',
+
+    'status_codes' => [
+        'ok' => '00',
+        'validation_failed' => '21',
+        'invalid_scope' => '22',
+        'authentication_error' => '23',
+        'permission_denied' => '24',
+        'not_found' => '25',
+        'forbidden' => '26',
+        'duplicated' => '27',
+        'timeout' => '28',
+        'unverified_account' => '29',
+        'limit_exceeded' => '30',
+        'invalid_assignment' => '31',
+        'phone_validation_failed' => '32',
+        'error' => '99',
+    ],
+
+    'http_codes' => [
+        'success' => '200',
+        'bad_request' => '400',
+        'unauthorized' => '401',
+        'forbidden' => '403',
+        'not_found' => '404',
+        'unprocessable_entity' => '422',
+        'internal_server_error' => '500',
+        'request_timeout' => '408',
+    ],
+
+    'frontend' => [
+        'url' => env('FRONTEND_URL'),
+        'reset_password_path' => env('FRONTEND_RESET_PASSWORD_PATH', '/reset?token={token}&email={email}'),
+        'set_password_path' => env('FRONTEND_SET_PASSWORD_PATH', '/set?token={token}&email={email}'),
+    ],
+];
