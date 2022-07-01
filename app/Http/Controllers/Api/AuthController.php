d762e9f8de9dc539393e287a44c0fd27aaa6d5fe
hafizihamidrdma <mohamadhafizihamid@gmail.com>
2022-07-01 16:56:32 +0800

    Fix: code formatting

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Requests\LoginRequest;
 use App\Services\AuthService;
 
--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Requests;
+
 use App\Http\Requests\ApiRequest;
 
 class LoginRequest extends ApiRequest
