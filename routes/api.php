6e912cc796c5a22e83f67e3dd4963f3eb50769eb
hafizihamidrdma <mohamadhafizihamid@gmail.com>
2022-07-06 15:13:43 +0800

    Fix: route login function

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 // Admin
 Route::group(
     ['prefix' => 'admin', 'as' => 'admin.'],
     function () {
-        Route::post('/login', "AuthController@login")->name('login');
+        Route::post('/login', [AuthController::class, 'login']);
     }
 );
