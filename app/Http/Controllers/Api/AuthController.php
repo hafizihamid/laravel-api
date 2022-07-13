bf2d9fc3de6900cd37199c05e74be574635218ac
hafizihamidrdma <mohamadhafizihamid@gmail.com>
2022-07-13 13:28:44 +0800

    Add: forgot password function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\ForgotPasswordRequest;
 use App\Services\AuthService;
 
 class AuthController extends ApiController
@@ -27,6 +28,26 @@ class AuthController extends ApiController
         }
 
         return $this->formatDataResponse(
+            $data['message'],
+            $data['status'],
+            $data['http_code']
+        );
+    }
+
+    public function forgot(ForgotPasswordRequest $request)
+    {
+        $data = $this->authService->forgot($request);
+
+
+        if ($data['status'] != config('staticdata.status_codes.ok')) {
+            return $this->formatErrorResponse(
+                [$data['message']],
+                $data['status'],
+                $data['http_code']
+            );
+        }
+
+        return $this->formatGeneralResponse(
             $data['message'],
             $data['status'],
             $data['http_code']
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -83,6 +83,37 @@ class AuthService extends BaseService
         );
     }
 
+    public function forgot($credentials)
+    {
+        $user = User::where('email', $credentials['email'])->first();
+
+        if ($user) {
+            if ($user->is_disabled) {
+                return $this->formatGeneralResponse(
+                    config('messages.authentication.user_disabled'),
+                    config('staticdata.status_codes.forbidden'),
+                    config('staticdata.http_codes.forbidden')
+                );
+            }
+        }
+
+        try {
+            Password::sendResetLink($credentials->all());
+        } catch (\Exception $e) {
+            return $this->formatGeneralResponse(
+                $e->getMessage(),
+                config('staticdata.status_codes.error'),
+                config('staticdata.http_codes.internal_server_error')
+            );
+        }
+
+        return $this->formatGeneralResponse(
+            config("messages.authentication.authentication_reset_email_successful"),
+            config('staticdata.status_codes.ok'),
+            config('staticdata.http_codes.success')
+        );
+    }
+
     public function getUserWithRolesPermissions($user)
     {
         $userArray = $user->toArray();
